756e56b665b1fe78174c1483988daee306d74159
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-04-10 23:28:05 +0200

    Answers.groovy

---
+++ src/test/groovy/Answers.groovy
@@ -0,0 +1,36 @@
+/**
+ * Created by mtumilowicz on 2019-04-10.
+ */
+class Answers {
+    /*
+    static
+    	left(L left)
+    	right(R right)
+    	sequence(Iterable<? extends Either<? extends L,? extends R>> eithers)
+        sequenceRight(Iterable<? extends Either<? extends L,? extends R>> eithers)
+    method
+        bimap(Function<? super L,? extends X> leftMapper,
+         Function<? super R,? extends Y> rightMapper)
+        filter(Predicate<? super R> predicate)
+        flatMap(Function<? super R,? extends Either<L,? extends U>> mapper)
+        	fold(Function<? super L,? extends U> leftMapper,
+    Function<? super R,? extends U> rightMapper)
+    get()
+    	getLeft()
+    		left()
+    		map(Function<? super R,? extends U> mapper)
+    		mapLeft(Function<? super L,? extends U> leftMapper)
+    		orElse(Supplier<? extends Either<? extends L,? extends R>> supplier) 
+    		orElseRun(Consumer<? super L> action)
+    		peek(Consumer<? super R> action)
+    		peekLeft(Consumer<? super L> action) 
+    			right()
+    				swap()
+    				toOption()
+    				toTry()
+     
+     
+     
+     */
+    
+}
